b4925c2137460817e8debe770322bdeaf787aab5
AymanElsAhmed <31375408+AymanElsAhmed@users.noreply.github.com>
2022-01-10 11:59:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,15 +12,26 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, play;
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let play = true;
+const init = function() {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    play = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+};
+
 const switchPlayer = function() {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
     currentScore = 0;
@@ -28,6 +39,8 @@ const switchPlayer = function() {
     player0El.classList.toggle('player--active');
     player1El.classList.toggle('player--active');
 };
+
+init();
 
 btnRoll.addEventListener('click', function() {
     if (play) {
@@ -51,6 +64,7 @@ btnHold.addEventListener('click', function() {
             scores[activePlayer];
         if (scores[activePlayer] >= 100) {
             play = false;
+            diceEl.classList.add('hidden');
             document
                 .querySelector(`.player--${activePlayer}`)
                 .classList.add('player--winner');
@@ -62,3 +76,11 @@ btnHold.addEventListener('click', function() {
         }
     }
 });
+
+btnNew.addEventListener('click', init);
+// if (
+//     player1El.classList.contains('player--active')
+// ) {
+//     player1El.classList.remove('player--active');
+//     player0El.classList.add('player--active');
+// }
